2d0eea878530b2478d5703f758f9225138e0debe
Aditya Mahaddalkar <AdityaMahaddalkar>
2020-11-28 03:41:48 +0000

    Initial commit

---
+++ src/test/java/io/company/QuestionnaireTest.java
@@ -0,0 +1,21 @@
+package io.company;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class QuestionnaireTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
